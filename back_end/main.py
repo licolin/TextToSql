55d734d594ff26d7e3a5ce5b75e31cc4354a2d28
colin <lizhaohuan026901@gtjas.com>
2024-12-05 17:49:47 +0800

    use huggeface

--- back_end/main.py
+++ back_end/main.py
@@ -1,13 +1,31 @@
-from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
+from langchain.llms import HuggingFaceHub
 
-app = FastAPI()
+load_dotenv()
 
+huggingface_api_key = os.getenv("HUGGINGFACE_API_KEY")
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+print("huggingface_api_key ", huggingface_api_key)
+model_name = os.getenv("MODEL_NAME")
 
+from huggingface_hub import login
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+login(token=huggingface_api_key)
+
+llm = HuggingFaceHub(
+    repo_id=model_name,
+    huggingfacehub_api_token=huggingface_api_key,
+    model_kwargs={"temperature": 0.7, "max_length": 512}
+)
+
+# llm = HuggingFaceHub(
+#     repo_id=model_name,
+#     model_kwargs={
+#         "temperature": 0.7,
+#         "max_length": 512
+#     }
+# )
+
+response = llm("What is the capital of France?")
+print(response)
